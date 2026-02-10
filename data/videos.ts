2921259c8d3923f90e389d1fe92956bf2e8e4634
Khurshid Computers <tumhara-github-email@example.com>
2026-02-10 23:22:28 +0500

    push

--- data/videos.ts
+++ data/videos.ts
@@ -2,17 +2,19 @@
 // You can reorder or remove any of these later if you like.
 
 export const GALLERY_VIDEOS = [
-  { id: 1, src: '/WhatsApp Video 2026-02-10 at 7.18.13 PM.mp4' },
-  { id: 2, src: '/WhatsApp Video 2026-02-10 at 7.19.27 PM.mp4' },
-  { id: 3, src: '/WhatsApp Video 2026-02-10 at 7.21.27 PM.mp4' },
-  { id: 4, src: '/WhatsApp Video 2026-02-10 at 7.21.33 PM.mp4' },
-  { id: 5, src: '/WhatsApp Video 2026-02-10 at 7.21.34 PM.mp4' },
-  { id: 6, src: '/WhatsApp Video 2026-02-10 at 7.21.35 PM.mp4' },
-  { id: 7, src: '/WhatsApp Video 2026-02-10 at 7.21.36 PM.mp4' },
-  { id: 8, src: '/WhatsApp Video 2026-02-10 at 7.21.37 PM.mp4' },
-  { id: 9, src: '/WhatsApp Video 2026-02-10 at 7.21.38 PM.mp4' },
-  { id: 10, src: '/WhatsApp Video 2026-02-10 at 7.21.39 PM 7.mp4' },
-  { id: 11, src: '/WhatsApp Video 2026-02-10 at 7.21.39 PM.mp4' },
-  { id: 12, src: '/WhatsApp Video 2026-02-10 at 7.21.40 PM 9.mp4' },
+  { id: 1, src: '/video 1.png.mp4 ' },
+  { id: 2, src: '/video 3.png.mp4' },
+  { id: 3, src: '/video 2.png.mp4' },
+  { id: 4, src: '/video 4.png.mp4' },
+  { id: 5, src: '/video 5.png.mp4' },
+  { id: 6, src: '/video 6.png.mp4' },
+  { id: 7, src: '/video 7.png.mp4' },
+  { id: 8, src: '/video 8.png.mp4' },
+  { id: 9, src: '/video 9.png.mp4' },
+  { id: 10, src: '/video 10.png.mp4' },
+  { id: 11, src: '/video 11.png.mp4' },
+  { id: 12, src: '/video 12.png.mp4' },
+  { id: 12, src: '/video 13.png.mp4' },
+  { id: 12, src: '/video 14.png.mp4' },
 ];
 
